e472732465a398b3b4ed005aa776ac3d80ed7868
Le Anh Thien <thien.lah@gmail.com>
2018-07-21 00:30:17 +0700

    Modify some issues

--- BflagClient/app/src/main/java/com/example/minhquan/bflagclient/chat/ChatActivity.kt
+++ BflagClient/app/src/main/java/com/example/minhquan/bflagclient/chat/ChatActivity.kt
@@ -9,5 +9,11 @@ class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+
+    }
+
+    fun setup() {
+
     }
 }
