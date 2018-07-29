06fe192f986e8c1548eee92ba7ba324e3510cb93
DESKTOP-6LFEV0I\quynh <quynhdx0305@gmail.com>
2018-07-29 16:08:51 +0700

    readme room ok

--- BflagClient/app/src/main/java/com/example/minhquan/bflagclient/home/HomePresenter.kt
+++ BflagClient/app/src/main/java/com/example/minhquan/bflagclient/home/HomePresenter.kt
@@ -1,5 +1,6 @@
 package com.example.minhquan.bflagclient.home
 
+import android.preference.Preference
 import com.example.minhquan.bflagclient.model.SuccessResponse
 import com.example.minhquan.bflagclient.model.User
 import com.example.minhquan.bflagclient.utils.CallbackWrapper
@@ -34,6 +35,7 @@ class HomePresenter(val view: HomeContract.View): HomeContract.Presenter {
                 .subscribeWith(object: CallbackWrapper<User>(view) {
                     override fun onSuccess(result: User) {
                         view.onGetUserSuccess(result)
+
                     }
                 })
     }
